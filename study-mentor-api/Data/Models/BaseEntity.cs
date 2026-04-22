e9cd8a550b7d02a7ada391a411a87b3e0b5ae03a
Eugene <zderij508@gmail.com>
2026-04-22 11:50:21 +0300

    s-2 update BaseEntity class

--- study-mentor-api/Data/Models/BaseEntity.cs
+++ study-mentor-api/Data/Models/BaseEntity.cs
@@ -1,6 +1,10 @@
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 namespace study_mentor_api.Data.Models;
 
 public abstract class BaseEntity<TKey> : IEntity<TKey>  where TKey : notnull
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public TKey Id { get; protected set; } = default!;
 }
